9ff8e6fe71063cc1f984f1e44f630d7f211d312e
crisha0126 <manugotcrishamae_bsit@plmun.edu.ph>
2025-02-20 05:28:52 -0700

    MethodReturnTypes.java

---
+++ MethodReturnTypes.java
@@ -0,0 +1,21 @@
+public class MethodReturntypes { 
+    public static void main(string[] args) {
+        int value1 = 20;
+        int value2 = 30:
+        double result =
+        calculateAverage(value1, value2);
+        
+        System.out.println("The average is: 
+        " + result);
+        displayWelcomeMessage();
+    }
+    public static void
+    displayWelcomeMessage() {
+        System.out.println("welcome to our program!");
+    }
+    public static double 
+    calculateAverage(int num1, int num2) {
+     double average = (num1 + num2) / 2.0;
+    return average;
+    }
+    }
